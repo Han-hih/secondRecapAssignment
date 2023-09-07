f8ad1c3a943fd7bdd19fbd61813985c247949a75
한 <hih458@naver.com>
2023-09-07 22:37:58 +0900

    feat 서치바 생성
    서치바에 x 버튼, cancel 버튼 추가

--- secondRecapAssignment/secondRecapAssignment/ViewController.swift
+++ secondRecapAssignment/secondRecapAssignment/ViewController.swift
@@ -9,21 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let searchBar = {
+    let view = UISearchBar()
+        view.placeholder = "검색어를 입력해주세요"
+        view.showsCancelButton = true
+        view.setImage(UIImage(named: "isSearchNonW"), for: UISearchBar.Icon.search, state: .normal)
+        view.setImage(UIImage(named: "isCancel"), for: .clear, state: .normal)
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+        searchBar.delegate = self
+        view.addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+
         setNavigationBar()
         
+        setConstraints()
     }
-
+    
+ 
 
     func setNavigationBar() {
         self.navigationItem.title = "쇼핑 검색"
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
     }
     
-    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
     
 }
-
+extension ViewController: UISearchBarDelegate {
+    
+}
