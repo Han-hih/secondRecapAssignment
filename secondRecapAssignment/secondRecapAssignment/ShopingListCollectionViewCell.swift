d856fa49e7bc909350d7b40042852ccb21c39671
한 <hih458@naver.com>
2023-09-08 09:11:45 +0900

    add 쇼핑 관련 레이블 생성
    각 레이블에 필요한 설정들 추가

--- secondRecapAssignment/secondRecapAssignment/ShopingListCollectionViewCell.swift
+++ secondRecapAssignment/secondRecapAssignment/ShopingListCollectionViewCell.swift
@@ -45,8 +45,40 @@ class ShopingListViewControllerCell: BaseCollectionViewCell {
         return button
     }()
     
+    let mallNameLabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.sizeToFit()
+        label.font = .systemFont(ofSize: 10)
+        label.text = "[네이버 쇼핑몰]"
+        return label
+    }()
+    
+    let titleLabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 15)
+        label.numberOfLines = 2
+        label.text = "sddfaldfsdfasfdasfsdfaff"
+        return label
+    }()
+    //숫자가 커질 때 글자크기가 작아지게
+    let priceLabel = {
+        let label = UILabel()
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 25)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.4 // 줄어들 수 있는 비율
+        label.text = numberFormatter.string(from: 12344000) // 대략 100억 까지만 으로 가정
+
+        return label
+    }()
+    
     override func configure() {
-        [shoppingImageView, buttonView, heartButton].forEach {
+        [shoppingImageView, buttonView, heartButton, mallNameLabel, titleLabel, priceLabel].forEach {
             contentView.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -69,7 +101,19 @@ class ShopingListViewControllerCell: BaseCollectionViewCell {
             buttonView.widthAnchor.constraint(equalTo: buttonView.heightAnchor),
             //하트버튼
             heartButton.centerXAnchor.constraint(equalTo: buttonView.centerXAnchor),
-            heartButton.centerYAnchor.constraint(equalTo: buttonView.centerYAnchor)
+            heartButton.centerYAnchor.constraint(equalTo: buttonView.centerYAnchor),
+            //쇼핑몰네임
+            mallNameLabel.topAnchor.constraint(equalTo: shoppingImageView.bottomAnchor, constant: 5),
+            mallNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            mallNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            //제품이름 레이블
+            titleLabel.topAnchor.constraint(equalTo: mallNameLabel.bottomAnchor, constant: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: mallNameLabel.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: mallNameLabel.trailingAnchor),
+            //가격레이블
+            priceLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            priceLabel.leadingAnchor.constraint(equalTo: mallNameLabel.leadingAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: mallNameLabel.trailingAnchor)
         
         ])
     }
