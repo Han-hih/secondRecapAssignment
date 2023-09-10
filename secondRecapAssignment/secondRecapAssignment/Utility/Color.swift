810a93f9d1e027fb99dadb88ee8e48ba491dc0f8
한 <hih458@naver.com>
2023-09-10 13:44:02 +0900

    style 색상에 대한 enum 값 생성

---
+++ secondRecapAssignment/secondRecapAssignment/Utility/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  secondRecapAssignment
+//
+//  Created by 황인호 on 2023/09/10.
+//
+
+import UIKit
+
+enum LightColor {
+    static let background = UIColor.white
+    static let border = UIColor.black.cgColor
+    static let text = UIColor.black
+    static let selectBorder = UIColor.white.cgColor
+    static let selectText = UIColor.white
+    static let placeholder = UIColor.lightGray
+}
